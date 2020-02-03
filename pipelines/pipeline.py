e56a289b07b7710ddc76cb78bd6bdc18b87a2036
mkrkkinen <mikko.karkkainen@cgi.com>
2020-02-03 11:00:50 +0200

    changes

--- pipelines/pipeline.py
+++ pipelines/pipeline.py
@@ -1,13 +1,6 @@
 import yaml
 import os
 
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-except ModuleNotFoundError:
-    print("python-dotenv not installed. Not loading .env")
-    pass
-
 from azureml.core import (
     Datastore,
     RunConfiguration,
@@ -21,29 +14,28 @@ from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.pipeline.core import Pipeline
 from azureml.pipeline.steps import PythonScriptStep
 
-with open("conf.yaml", "r") as f:
-    conf = yaml.load(f, Loader=yaml.FullLoader)
-    auth_config = conf["auth"]
-    compute = conf["compute"]
+with open("azure-variables.yaml", "r") as f:
+    conf = yaml.safe_load(f, Loader=yaml.FullLoader)
+    variables = conf["variables"]
 
 # Authenticate with AzureML
-auth = ServicePrincipalAuthentication(
-    tenant_id=auth_config["tenant_id"],
-    service_principal_id=auth_config["service_principal_id"],
-    service_principal_password=os.environ["SP_SECRET"],
+#auth = ServicePrincipalAuthentication(
+    #tenant_id=variables["tenant_id"],
+    #service_principal_id=variables["service_principal_id"]
 )
 
 ws = Workspace(
-    subscription_id=auth_config["subscription_id"],
-    resource_group=auth_config["resource_group"],
-    workspace_name=auth_config["workspace_name"],
-    auth=auth,
+    subscription_id=variables["subscription_id"],
+    resource_group=variables["resource_group"],
+    workspace_name=variables["workspace_name"],   
 )
 
 # Usually, the  cluster already exists, so we just fetch
-compute_target = next(
-    (m for m in ComputeTarget.list(ws) if m.name == compute["name"]), None
-)
+try:
+    compute_target = ComputeTarget(ws, variables["AML_COMPUTE_CLUSTER_CPU_SKU"])
+    print('Found existing cluster, use it.')
+except ComputeTargetException:
+    compute_config = AmlCompute.provisioning_configuration(vm_size = variables['AML_COMPUTE_CLUSTER_SIZE'], max_nodes = variables['AML_CLUSTER_MAX_NODES'])
 
 run_config = RunConfiguration(
     conda_dependencies=CondaDependencies(
